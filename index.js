6299d811becec7cf1b0c54fcd63cf2cdd5550afb
Ken Tan <nedvedken@gmail.com>
2022-09-19 22:13:23 +0800

    Bugfix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 
 var app = express()
 
-app.get('/', (request, response) => {
+app.post('/', (request, response) => {
   const botToken = '5067222237:AAEqGd3C3MbsLN51G218zVTqJOeBFwYLI2s'
   const chatId = '1306821852'
 
