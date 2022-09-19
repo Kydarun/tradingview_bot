f3479e2d38a235e83c458448b35890f4ed1d9563
Ken Tan <nedvedken@gmail.com>
2022-09-19 20:42:09 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const axios = require('axios').default
+const http = require('http')
+const { DateTime } = require("luxon")
+
+const botToken = '5067222237:AAEqGd3C3MbsLN51G218zVTqJOeBFwYLI2s'
+const chatId = '1306821852'
+
+http.createServer((request, response) => {
+  let body = ''
+  request.on('data', chunk => {
+    body = `${body}${chunk}`
+  })
+  request.on('end', () => {
+    if (body) {
+        axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+        chat_id: chatId,
+        text: body
+      }).then((res) => {
+        console.log(res)
+        response.write('Y')
+        response.end()
+      })
+    }
+    else {
+      response.write(`Hello! It's ${DateTime.now().toLocaleString(DateTime.DATETIME_MED)}.`)
+      response.end()
+    }
+  })
+}).listen()
